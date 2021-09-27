d8ecad94209ebdad1462f83c2764118f4d4cdb22
Marko Nikolaš <markon@stuntcoders.com>
2021-09-27 11:10:37 +0200

    Add docker wait for db script

---
+++ docker_wait_for_db/wait-for.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+HOST=$(echo $WORDPRESS_DB_HOST | cut -d: -f1)
+PORT=$(echo $WORDPRESS_DB_HOST | cut -d: -f2)
+
+CMD=$@
+
+until mysql -h $HOST -P $PORT -D $WORDPRESS_DB_NAME -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -e '\q'; do
+  >&2 echo "Mysql is unavailable - sleeping..."
+  sleep 2
+done
+
+>&2 echo "Mysql is up - executing command"
+exec $CMD
+
+
